4ebd7eab8c9c7fde05e44f719ed48b09c8669d03
paulomiim <paulomimehta0208@yahoo.com>
2024-08-22 20:30:00 -0700

    Update Program5.java
    completed program 5.

--- Program5.java
+++ Program5.java
@@ -1,6 +1,67 @@
 //import stuff here?
 
 //Your code here
+public class Program5 {
+    public static void main (String[]args) {
+        //set variables
+        double royaleMiles = 286;
+        double royale = (double)royaleMiles / 9;
+        double koopaKingMiles = 412;
+        double koopaKing = (double)koopaKingMiles / 40;
+        double pipeFrameMiles = 361;
+        double pipeFrame = (double)pipeFrameMiles / 18;
+        double badwagonMiles = 161;
+        double badwagon = (double)badwagonMiles / 11;
+
+        //get rid of decimals
+        
+        //royale
+        royale = (royale * 10);
+        royale = (royale + 0.5);
+        royale = ((int)royale);
+        royale = royale / 10.0;
+        //koopaking
+        koopaKing = koopaKing * 10;
+        koopaKing = koopaKing + 0.5;
+        koopaKing = (int)koopaKing;
+        koopaKing = koopaKing / 10.0;
+        //pipeFrame
+        pipeFrame = pipeFrame * 10;
+        pipeFrame = pipeFrame + 0.5;
+        pipeFrame = (int)pipeFrame;
+        pipeFrame = pipeFrame / 10.0;
+        //badwagon
+        badwagon = badwagon * 10;
+        badwagon = badwagon + 0.5;
+        badwagon = (int)badwagon;
+        badwagon = badwagon / 10.0;
+        
+        
+        
+
+        //print results
+        
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
+        System.out.println("Royale averaged " + royale + " m/g");
+        System.out.println("Koopa King averaged " + koopaKing + " m/g");
+        System.out.println("Pipe Frame averaged " + pipeFrame + " m/g");
+        System.out.println("Badwagon averaged " + badwagon + " m/g");
+        
+        
+    }
+}
+//Paste console output below:
+/*
+Mushroom Cup Prix Racer Average Miles/Per Gallon:
+Royale averaged 31.8 m/g
+Koopa King averaged 10.3 m/g
+Pipe Frame averaged 20.1 m/g
+Badwagon averaged 14.6 m/g
+
+*/
+//import stuff here?
+
+//Your code here
 
 //Paste console output below:
 /*
